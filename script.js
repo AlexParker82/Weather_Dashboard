022fe5c844bd4a861515810d89fa4cf63ec8a99b
Alex Parker <alex.n.parker82@gmail.com>
2021-08-01 22:52:20 -0400

    working on local storage

--- script.js
+++ script.js
@@ -1,30 +1,87 @@
-
-    const resultCard$ = $('.current');
+$(function () {
+    const searchSideBar$ = $('.sidebar');
+    const savedSideBar$ = $('.sidebar-saved');
+    const resultCard$ = $('.card');
     const cardTitle$ = $('.card-title')
     const cardSubtitle$ = $('.card-subtitle');
     const displayTemp$ = $('#temp');
     const displayWind$ = $('#wind');
     const displayHumidity$ = $('#humidity');
+    const userInput$ = $('.form-control');
+    const newBtn$ = $('.btn-primary');
+    const storageArray$ = [];
+    const cityInfo$ = JSON.parse(localStorage.getItem('cityInfo'));
 
-    function getApi() {
-        let url = "http://api.openweathermap.org/data/2.5/weather?q=Boston&units=imperial&id=524901&appid=a1d66d99e4fef075a52f8388265e5590";
+    function getCurrentApi() {
+        let userInput = userInput$.val();
+        let url = `http://api.openweathermap.org/data/2.5/weather?q=${userInput}&units=imperial&id=524901&appid=a1d66d99e4fef075a52f8388265e5590`;
 
         $.get(url)
-            .then(function(data) {
-                console.log(data.name);
-                let date = moment.unix(data.dt).format("dddd, MMMM Do YYYY");
+            .fail(function () {
+                alert('404 Error');
+            })
 
-                cardTitle$.text(data.name);
-                cardSubtitle$.text(date);
-                displayTemp$.text(data.main.temp);
-                displayWind$.text(data.wind.speed);
-                displayHumidity$.text(data.main.humidity);
+            .done(function (data) {
+                console.log(data.coord);
+                if (userInput === "") {
+                    let alert = $('<h4>').addClass('alert');
+                    alert.text('Enter a City!');
+                    searchSideBar$.append(alert);
+                    $('h4').remove('.alert');
+                } else {
+                    $('h4').remove('.alert');
+                    let cityObj = {
+                        city: userInput,
+                        lat: data.coord.lat,
+                        lon: data.coord.lon
+                    }
+
+                    storageArray$.push(cityObj);
+
+                    localStorage.setItem('cityInfo', JSON.stringify(storageArray$));
+
+                    let date = moment.unix(data.dt).format("dddd, MMMM Do YYYY");
+                    resultCard$.addClass('show');
+                    cardTitle$.text(data.name);
+                    cardSubtitle$.text(date);
+                    displayTemp$.text(data.main.temp);
+                    displayWind$.text(data.wind.speed);
+                    displayHumidity$.text(data.main.humidity);
+                }
 
             });
     };
 
+    function RenderSavedBtns() {
 
-    getApi();
+        console.log(cityInfo$);
+        let cityBtn = document.createElement('button');
+        cityBtn.className = "btn btn-secondary";
+        cityBtn.innerHTML = cityInfo$.city;
+        savedSideBar$.append(cityBtn);
+
+    }
+
+
+
+    RenderSavedBtns();
+
+    newBtn$.click(function () {
+        getCurrentApi();
+    })
+});
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
